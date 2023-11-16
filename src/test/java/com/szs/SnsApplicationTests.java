fe7022f2b591021a8498bd3ac5dd2f6fdaeca207
hbpyun <hbpyun@fssunivserse.com>
2023-11-17 03:27:28 +0900

    누락파일 추가

---
+++ src/test/java/com/szs/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
